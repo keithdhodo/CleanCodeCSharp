012f513a871fa8af4389290112c230436113f821
ssfcultra <koolkeith13@gmail.com>
2017-10-15 09:26:47 -0700

    Finished verbose version of Sieve of Eratosthenes.

--- Chapter04/SieveOfEratosthenes.cs
+++ Chapter04/SieveOfEratosthenes.cs
@@ -19,9 +19,59 @@ namespace Chapter04
     /// </summary>
     public class SieveOfEratosthenes
     {
-        public List<BigInteger> GeneratePrimes(BigInteger maxValue)
+        public List<int> GeneratePrimes(int maxValue)
         {
-            var primes = new List<BigInteger>();
+            var primes = new List<int>();
+
+            if (maxValue >= 2) // the only valid case
+            {
+                // declarations
+                int s = maxValue + 1; // size of the List
+                bool[] f = new bool[s];
+
+                // initialize the array to true
+                for(int i = 0; i < s; i++)
+                {
+                    f[i] = true;
+                }
+
+                // get rid of the non-primes
+                f[0] = f[1] = false;
+
+                // sieve
+                for (int i = 2; i < Math.Sqrt(s) + 1; i++)
+                {
+                    if (f[i]) // i is uncrossed. Cross its multiples
+                    {
+                        for (int j = 2 * i; j < s; j += i)
+                        {
+                            f[j] = false; // multiple is not prime
+                        }
+                    }
+                }
+
+                // how many primes are there
+                int count = 0;
+                for (int i = 0; i < s; i++)
+                {
+                    if (f[i])
+                    {
+                        count++; // bump count
+                    }
+                }
+
+                // re-initialize the List to proper count
+                primes = new List<int>(count);
+
+                // move the primes into the list
+                for (int i = 0; i < s; i++)
+                {
+                    if (f[i]) // if prime
+                    {
+                        primes.Add(i);
+                    }
+                }
+            }
 
             return primes;
         }
