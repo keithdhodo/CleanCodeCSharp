be0f0a7ac8ea926660279a4315268f615b230b9a
ssfcultra <koolkeith13@gmail.com>
2017-10-19 21:15:49 -0700

    Added Quadratic class

---
+++ Chapter05/Quadratic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter05
+{
+    public class Quadratic
+    {
+        public static double Root(double a, double b, double c)
+        {
+            var determinant = Determinant(a, b, c);
+            return (-b + Math.Sqrt(determinant)) / (2 * a);
+        }
+
+        public static double Root(int a, int b, int c)
+        {
+            var determinant = Determinant(a, b, c);
+            return (-b + Math.Sqrt(determinant)) / (2 * a);
+        }
+
+        private static double Determinant(double a, double b, double c)
+        {
+            return (b * b) - (4 * a * c);
+        }
+    }
+}
---
+++ Chapter05Tests/QuadraticTests.cs
@@ -0,0 +1,50 @@
+﻿using Chapter05;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter05Tests
+{
+    [TestClass]
+    public class QuadraticTests
+    {
+        [TestMethod]
+        public void Quadratic_Test_Double_OneTwoThree()
+        {
+            var expected = double.NaN;
+            var actual = Quadratic.Root(1.0, 2.0, 3.0);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Quadratic_Test_Double_ThreeOneHundredOne()
+        {
+            var expected = Math.Round(-0.0100030018013513, 15);
+            var actual = Math.Round(Quadratic.Root(3.0, 100.0, 1.0), 15);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Quadratic_Test_Int_OneTwoThree()
+        {
+            var expected = double.NaN;
+            var actual = Quadratic.Root(1, 2, 3);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Quadratic_Test_Int_ThreeOneHundredOne()
+        {
+            var expected = Math.Round(-0.0100030018013513, 15);
+            var actual = Math.Round(Quadratic.Root(3, 100, 1), 15);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
