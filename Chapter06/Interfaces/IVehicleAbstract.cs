0f2da1aa22a20166b609ae114491902306bf5bca
ssfcultra <koolkeith13@gmail.com>
2017-10-22 19:58:24 -0700

    Added two interfaces for vehicles. One concrete and one abstract.

---
+++ Chapter06/Interfaces/IVehicleAbstract.cs
@@ -0,0 +1,7 @@
+﻿namespace Chapter06.Interfaces
+{
+    public interface IVehicleAbstract
+    {
+        double GetPercentFuelRemaining();
+    }
+}
---
+++ Chapter06/Interfaces/IVehicleConcrete.cs
@@ -0,0 +1,8 @@
+﻿namespace Chapter06.Interfaces
+{
+    public interface IVehicleConcrete
+    {
+        double GetFuelTankCapacityInGallons();
+        double GetGallonsOfGasoline();
+    }
+}
