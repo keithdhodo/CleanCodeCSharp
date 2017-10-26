5979aac546ebdbb715705f5e9a478874c8c0e124
Keith Hodo <keith.hodo@premera.com>
2017-10-25 18:25:55 -0700

    Added a basic example of a somewhat clean try/catch block.

--- Chapter07/SectionStore.cs
+++ Chapter07/SectionStore.cs
@@ -1,11 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Chapter07
 {
     public class SectionStore
     {
-        public List<RecordedGrip> RetrieveSection()
+        public List<RecordedGrip> RetrieveSection(string SectionName)
         {
+            try
+            {
+                var stream = new FileStream(SectionName, FileMode.Open);
+            }
+            catch(Exception ex)
+            {
+                throw new IOException("retrieval error", ex);
+            }
+
             return new List<RecordedGrip>();
         }
     }
--- Chapter07Tests/SectionStoreTests.cs
+++ Chapter07Tests/SectionStoreTests.cs
@@ -1,15 +1,18 @@
 using Chapter07;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
 
 namespace Chapter07Tests
 {
     [TestClass]
     public class SectionStoreTests
     {
+        [ExpectedException(typeof(IOException))]
         [TestMethod]
         public void RecordedGrip_RetrieveSectionShouldThrowInvalidFileName()
         {
-            new SectionStore().RetrieveSection();
+            new SectionStore().RetrieveSection("foo");
         }
     }
 }
